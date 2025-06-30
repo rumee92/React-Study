d73543e2dfdc5d02dac45a7ec0d1f40c061f89b6
rumee92 <joaruem92@gmail.com>
2025-06-30 22:28:36 +0900

    github 배포 설정

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/React-Study/', // 예시: 저장소 이름이 my-react-app 이면 /my-react-app/
   plugins: [react()],
-})
+});
